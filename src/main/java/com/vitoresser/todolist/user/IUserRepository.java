bb672b0ff0b9fb1563fa3e50869581577a29cb41
VitorEsser <vitoresser10@gmail.com>
2023-10-11 20:34:48 +0200

    class 2 - database integration

---
+++ src/main/java/com/vitoresser/todolist/user/IUserRepository.java
@@ -0,0 +1,14 @@
+package com.vitoresser.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+  // UserModel findByUsername(String username);
+
+  @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM tb_users u WHERE u.username = :username")
+    boolean existsByUsername(@Param("username") String username);
+}
